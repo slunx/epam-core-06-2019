1e53663b484798c640c61074d4cc3a5c17e03530
slunx <slunx921@gmail.com>
2019-06-23 16:55:06 +0300

    Initial commit

---
+++ src/main/java/com/epam/classwork/se01/Example1.java
@@ -0,0 +1,68 @@
+package com.epam.classwork.se01;
+
+import javafx.scene.image.Image;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import javax.swing.*;
+
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        //       int[] arr = {1,2,3,4,5,6,7};
+//        for (int x = 0; x < 2_000_000; x++){
+//            ArrayList<int[]> arr2 = new ArrayList<>();
+//            arr2.add(arr);
+//           System.out.println(x);
+//           try {
+//               Thread.sleep(1000);
+//           } catch (InterruptedException ex){
+//               System.out.println("Thread is interruption");
+//           }
+//        }
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException ex) {
+//            System.out.println("Thread is interruption");
+//        }
+
+
+
+        // start();
+//       for (int i = 1; i < 10; i++) {
+//           System.out.println(i);
+//           try {
+//               TimeUnit.SECONDS.sleep(2);
+//           } catch (InterruptedException ex) {
+//
+//           }
+       }
+
+
+//
+//    private static void start() {
+//        Thread thread = new Thread();
+//        //thread.run();
+//        thread.start();
+//        System.out.println(thread.getName());
+//        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+//        for (int x = 0; x < 2_000_000; x++) {
+//            ArrayList<int[]> arr2 = new ArrayList<>();
+//            arr2.add(arr);
+//            System.out.println(x);
+//            try {
+//            thread.sleep(1000);
+//        } catch (InterruptedException ex) {
+//            System.out.println("Thread is interruption");
+//        }
+//        }
+//    }
+    }
+
+
+
+
+
